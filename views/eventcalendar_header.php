bd56e9000ad04d383b1914ddfb9c97cf7571c906
trk <iskender@6ve1.com>
2012-07-05 19:15:29 +0300

    No need more deleted

--- views/eventcalendar_header.php
+++ views/eventcalendar_header.php
@@ -1,11 +1,11 @@
 <ion:eventcalendar>
     <!-- Start Event Calendar -->
-    <link rel='stylesheet' type='text/css' href='<ion:base_url />modules/Eventcalendar/lib/styles/fullcalendar.css' />
-    <link rel='stylesheet' type='text/css' href='<ion:base_url />modules/Eventcalendar/lib/styles/fullcalendar.print.css' media='print' />
-    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/lib/javascripts/jquery-1.6.min.js'></script>
-    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/lib/javascripts/jquery-ui-1.8.11.custom.min.js'></script>
-    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/lib/javascripts/fullcalendar.min.js'></script>
-    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/lib/javascripts/wtooltip.min.js'></script>
+    <link rel='stylesheet' type='text/css' href='<ion:base_url />modules/Eventcalendar/assets/css/fullcalendar.css' />
+    <link rel='stylesheet' type='text/css' href='<ion:base_url />modules/Eventcalendar/assets/css/fullcalendar.print.css' media='print' />
+    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/assets/js/jquery-1.6.min.js'></script>
+    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/assets/js/jquery-ui-1.8.11.custom.min.js'></script>
+    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/assets/js/fullcalendar.min.js'></script>
+    <script type='text/javascript' src='<ion:base_url />modules/Eventcalendar/assets/js/wtooltip.min.js'></script>
     <script type='text/javascript'>
     	$(document).ready(function() {
             var date = new Date();
@@ -156,3 +156,29 @@
         });
     </script>
 </ion:eventcalendar>
+<!-- End Event Calendar -->
+<div id="legend_calendar" style="border: 1px solid #CCCCCC; border-radius: 5px; padding: 10px; margin-bottom: 20px;">
+	<ion:eventcalendar>
+		<ion:categories>
+			<div id="category_<ion:category_id/>" style="display: inline-block; padding: 0 10px;">
+			<div class="squarecolor" style="display: inline-block; width:16px; height:16px; background:<ion:category_color/>;">&nbsp;</div>
+			<ion:category_name/>
+			</div>
+		</ion:categories>
+	</ion:eventcalendar>
+</div>
+<select id="monthlist">
+	<option value="0" <?php if(date('n')-1 == 0) echo 'selected="selected"'?>><ion:translation term="january" /></option>
+	<option value="1" <?php if(date('n')-1 == 1) echo 'selected="selected"'?>><ion:translation term="february" /></option>
+	<option value="2" <?php if(date('n')-1 == 2) echo 'selected="selected"'?>><ion:translation term="march" /></option>
+	<option value="3" <?php if(date('n')-1 == 3) echo 'selected="selected"'?>><ion:translation term="april" /></option>
+	<option value="4" <?php if(date('n')-1 == 4) echo 'selected="selected"'?>><ion:translation term="may" /></option>
+	<option value="5" <?php if(date('n')-1 == 5) echo 'selected="selected"'?>><ion:translation term="june" /></option>
+	<option value="6" <?php if(date('n')-1 == 6) echo 'selected="selected"'?>><ion:translation term="july" /></option>
+	<option value="7" <?php if(date('n')-1 == 7) echo 'selected="selected"'?>><ion:translation term="august" /></option>
+	<option value="8" <?php if(date('n')-1 == 8) echo 'selected="selected"'?>><ion:translation term="september" /></option>
+	<option value="9" <?php if(date('n')-1 == 9) echo 'selected="selected"'?>><ion:translation term="october" /></option>
+	<option value="10" <?php if(date('n')-1 == 10) echo 'selected="selected"'?>><ion:translation term="november" /></option>
+	<option value="11" <?php if(date('n')-1 == 11) echo 'selected="selected"'?>><ion:translation term="december" /></option>
+</select>
+<div id="calendar"></div>
