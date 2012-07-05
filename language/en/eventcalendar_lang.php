cdea8d40a91fed3ca4a286f26ccecf6a25a34271
trk <iskender@6ve1.com>
2012-07-05 14:03:35 +0300

    lang item added

--- language/en/eventcalendar_lang.php
+++ language/en/eventcalendar_lang.php
@@ -6,6 +6,10 @@ $lang['module_eventcalendar_subtitle']              = 'You can create multi lang
 $lang['module_eventcalendar_title_categories']      = 'Categories';
 $lang['module_eventcalendar_subtitle_categories']   = 'You can manage module categories easily!';
 
+// Toolbox
+$lang['module_eventcalendar_toolbox_eventcalendar'] = 'Event Calendar';
+$lang['module_eventcalendar_toolbox_categories']    = 'Categories';
+
 // Event Calendar Labels
 $lang['module_eventcalendar_label_add_new_event']   = 'Add New Event';
 $lang['module_eventcalendar_label_event_date']      = 'Event Date';
@@ -13,26 +17,12 @@ $lang['module_eventcalendar_label_event_start_date']= 'Start Date';
 $lang['module_eventcalendar_label_event_end_date']  = 'End Date';
 $lang['module_eventcalendar_label_article']         = 'Article';
 $lang['module_eventcalendar_label_display_lang']    = 'Displayed Language';
+$lang['module_eventcalendar_label_none']      = '-- NONE --';
 
 // Event Calendar Category Labels
 $lang['module_eventcalendar_label_add_new_category'] = 'Add New Category';
 $lang['module_eventcalendar_label_color']            = 'Color';
-
-
-$lang['module_eventcalendar_event_added']           = 'Event Successfully Saved';
-$lang['module_eventcalendar_event_nadded']          = 'Event Not Saved, Event Name And Event Dates Are Required Fields';
-$lang['module_eventcalendar_event_deleted']         = 'Event Successfully Deleted';
-$lang['module_eventcalendar_event_ndeleted']        = 'Event Not Deleted';
-$lang['module_eventcalendar_edit_event']            = 'Edit Event';
-$lang['module_eventcalendar_event_list']            = 'Event List';
-$lang['module_eventcalendar_event_category']        = 'Event Category';
-
-$lang['module_eventcalendar_label_category_name']   = 'Category Name';
-
-// Toolbox
-$lang['module_eventcalendar_toolbox_eventcalendar'] = 'Event Calendar';
-$lang['module_eventcalendar_toolbox_categories']    = 'Categories';
-
+$lang['module_eventcalendar_label_color_picker_container'] = 'Color picker container';
 
 // Callback Messages (Event Calendar)
 $lang['module_eventcalendar_callback_event_saved']    = 'Event successfully saved!';
@@ -47,9 +37,7 @@ $lang['module_eventcalendar_callback_event_category_deleted'] = 'Event category 
 $lang['module_eventcalendar_callback_event_category_ndeleted']    = 'Event category could not delete!';
 $lang['module_eventcalendar_callback_event_category_in_use']    = 'Category in use by event calendar, please delete events or change event category!';
 
-
-
-
+// Old Translations
 $lang['module_eventcalendar_event_name']            = 'Event Name';
 $lang['module_eventcalendar_event_date']            = 'Event Date';
 $lang['module_eventcalendar_event_start_date']      = 'Start Date';
@@ -65,6 +53,7 @@ $lang['module_eventcalendar_event_ndeleted']        = 'Event Not Deleted';
 $lang['module_eventcalendar_edit_event']            = 'Edit Event';
 $lang['module_eventcalendar_event_list']            = 'Event List';
 $lang['module_eventcalendar_event_category']        = 'Event Category';
+
 // Category
 $lang['module_eventcalendar_category_list']         = 'Category List';
 $lang['module_eventcalendar_category_added']        = 'Category Successfully Saved';
