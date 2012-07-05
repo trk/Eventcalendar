cbd52edc47979dab9dacd1344a4f6961b401c2a9
trk <iskender@6ve1.com>
2012-07-05 15:03:21 +0300

    _get_events() function moved to model file

--- models/eventcalendar_model.php
+++ models/eventcalendar_model.php
@@ -6,6 +6,9 @@ class Eventcalendar_model extends Base_model {
         
         parent::__construct();
         
+        $this->load->model('Eventcalendarcategory_model', 'eventcalendarcategory_model', true);
+        $this->load->model('Article_model', 'article_model', true);
+        
         // Event Calendar Tables
         $this->table        = 'module_eventcalendar';
         $this->lang_table   = 'module_eventcalendar_lang';
@@ -15,6 +18,36 @@ class Eventcalendar_model extends Base_model {
         $this->category_table        = 'module_eventcalendar_category';
         $this->category_lang_table   = 'module_eventcalendar_category_lang';
         $this->category_pk_name      = 'id_category';
+    }
+    
+    function _get_events($where = FALSE, $lang = FALSE) {
+        
+        $lang = ($lang === FALSE) ? FALSE : $lang;
+        $where = ($where === FALSE) ? FALSE : $where;
+        
+        $data = array();
+        
+        $events     = $this->eventcalendar_model->get_lang_list($where, $lang);
+        $categories = $this->eventcalendarcategory_model->get_lang_list(FALSE, $lang);
+        $articles   = $this->article_model->get_lang_list(FALSE, $lang);
+        
+        if(count($events > 0)) {
+            
+            $data = $events;
+            
+            foreach ($data as $Ekey => $Evalue) {
+                if($Evalue['id_category'] != 0 && $Evalue['id_category'] != '')
+                    foreach ($categories as $Ckey => $Cvalue)
+                        if($Cvalue['id_category'] == $Evalue['id_category'] && $Cvalue['lang'] == $Evalue['lang'])
+                            $data[$Ekey]['category'] = $Cvalue;
+                if($Evalue['id_article'] != 0 && $Evalue['id_article'] != '')
+                    foreach ($articles as $Akey => $Avalue)
+                        if($Avalue['id_article'] == $Evalue['id_article'] && $Avalue['lang'] == $Evalue['lang'])
+                            $data[$Ekey]['article'] = $Cvalue;
+            }
+        }
+        
+        return $data;
     }
     
    /**
