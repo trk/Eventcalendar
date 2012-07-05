aedf78dddfda6c2b650469e56cab396c8ff57720
trk <iskender@6ve1.com>
2012-07-05 13:52:37 +0300

    Old functions changed with new

--- controllers/admin/eventcalendarcategory.php
+++ controllers/admin/eventcalendarcategory.php
@@ -1,7 +1,10 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Eventcalendarcategory extends Module_Admin {
-
+    
+    public $data = '';
+    public $lang_data = '';
+    
     function construct() {
         
         // Controller URL & Controller Fodler
@@ -9,7 +12,13 @@ class Eventcalendarcategory extends Module_Admin {
     	$this->controller_folder = 'admin/eventcalendarcategory/';
         
         // Load Event Calendar Model File
+        $this->load->model('Eventcalendar_model', 'eventcalendar_model', true);
         $this->load->model('Eventcalendarcategory_model', 'eventcalendarcategory_model', true);
+        
+        // Controller Database Tables
+        $this->table        = 'module_eventcalendar_category';
+        $this->lang_table   = 'module_eventcalendar_category_lang';
+        $this->pk_name      = 'id_category';
 		
         // Send Controller URL to Templates
         $this->template['controller_url'] = $this->controller_url;
@@ -17,117 +26,136 @@ class Eventcalendarcategory extends Module_Admin {
     
     function index() {
         
+        $this->template['categories'] = $this->eventcalendarcategory_model->get_lang_list(FALSE, Settings::get_lang());
+        
         $this->output($this->controller_folder . 'index');
     }
     
-    /**
-     * Save Category
-     */
     function save() {
-        $categoryID = $this->input->post('id_category');
 
-        // CREATION
-        if (empty($categoryID)) {
-            if ($this->input->post('category_name') != '' && $this->input->post('category_color') != '') {
-                $data = array(
-                    'name' => $this->input->post('category_name'),
-                    'color' => $this->input->post('category_color'),
-                );
-                $categoryID = $this->eventcalendar->saveCategory($data);
+        $id_category = ($this->input->post('id_category')) ? $this->input->post('id_category') : '';
 
-                // CALLBACK
-                $this->callback = array(
-                    array(
-                        'fn' => 'ION.updateElement',
-                        'args' => array(
-                            'element' => 'mainPanel',
-                            'url' => 'module/eventcalendar/eventcalendar/index'
-                        )
-                    ),
-                    array(
+        $id_article = ($this->input->post('id_article')) ? $this->input->post('id_article') : '';
+
+        $this->_prepare_data();
+        // trace($this->lang_data);
+        if ($this->eventcalendarcategory_model->save($this->data, $this->lang_data)) {
+            
+            $this->callback[] = array(
+                'fn' => 'ION.updateElement',
+                'args' => array(
+                    'element'=> 'mainPanel',
+                    'url' => $this->controller_url . 'index'
+                )
+            );
+
+            $this->callback[] = array(
+                'fn' => 'ION.notification',
+                'args' => array('success', lang('module_eventcalendar_callback_event_category_saved'))
+            );
+
+            $this->response();
+            
+        } else {
+
+            $this->callback[] = array(
+                'fn' => 'ION.notification',
+                'args' => array('error', lang('module_eventcalendar_callback_event_category_nsaved'))
+            );
+
+            $this->response();
+        }
+    }
+    
+    function edit($id = NULL) {
+
+        $id = (!is_null($id)) ? $id : $this->input->post('id_category');
+
+        $this->eventcalendarcategory_model->feed_template($id, $this->template);
+        $this->eventcalendarcategory_model->feed_lang_template($id, $this->template);
+
+        $this->template['category'] = $this->eventcalendarcategory_model->get_row_array($id);
+
+        $this->output($this->controller_folder . 'edit');
+    }
+    
+    function delete($id) {
+
+        Cache()->clear_cache();
+
+        if ($id != '') {
+            
+            if(count($this->eventcalendar_model->get_list(array('id_category' => $id))) === 0) {
+                // Delete Data
+                if ($this->eventcalendarcategory_model->delete($id)) {
+
+                    // Delete lang data
+                    $this->eventcalendarcategory_model->delete(array($this->pk_name => $id), $this->lang_table);
+
+                    /** Remove deleted items from DOM * */
+                    $this->callback[] = array(
+                        'fn' => 'ION.deleteDomElements',
+                        'args' => array('.category' . $id)
+                    );
+
+                    $this->success(lang('module_eventcalendar_callback_event_category_deleted'));
+
+                    $this->response();
+                } else {
+                    // Send Error Message
+                    $this->callback[] = array
+                        (
                         'fn' => 'ION.notification',
-                        'args' => array('success', lang('module_eventcalendar_category_added'))
-                    )
-                );
-                $this->response();
+                        'args' => array('error', lang('module_eventcalendar_callback_event_category_ndeleted'))
+                    );
+
+                    $this->response();
+                }
             } else {
-                // CALLBACK
-                $this->callback[] = array
-                    (
-                    'fn' => 'ION.notification',
-                    'args' => array('error', lang('module_eventcalendar_category_nadded'))
-                );
-                $this->response();
+                // Send Error Message
+                    $this->callback[] = array
+                        (
+                        'fn' => 'ION.notification',
+                        'args' => array('error', lang('module_eventcalendar_callback_event_category_in_use'))
+                    );
+
+                    $this->response();
             }
         } else {
-            // UPDATE
-            if ($this->input->post('category_name') != '' && $this->input->post('category_color') != '') {
-                $data = array(
-                    'name' => $this->input->post('category_name'),
-                    'color' => $this->input->post('category_color'),
-                );
-                $categoryID = $this->eventcalendar->updateCategory($categoryID, $data);
 
-                // CALLBACK
-                $this->callback = array(
-                    array(
-                        'fn' => 'ION.updateElement',
-                        'args' => array(
-                            'element' => 'mainPanel',
-                            'url' => 'module/eventcalendar/eventcalendar/index'
-                        )
-                    ),
-                    array(
-                        'fn' => 'ION.notification',
-                        'args' => array('success', lang('module_eventcalendar_category_added'))
-                    )
-                );
-                $this->response();
-            } else {
-                // CALLBACK
-                $this->callback[] = array
-                    (
-                    'fn' => 'ION.notification',
-                    'args' => array('error', lang('module_eventcalendar_category_nadded'))
-                );
-                $this->response();
+            // Send Error Message
+            $this->callback[] = array
+                (
+                'fn' => 'ION.notification',
+                'args' => array('error', lang('module_eventcalendar_callback_event_category_ndeleted'))
+            );
+
+            $this->response();
+        }
+    }
+    
+    /**
+     * Prepare data before saving
+     **/
+    function _prepare_data($xhr = FALSE) {
+        // Standard fields
+        $fields = $this->db->list_fields($this->table);
+
+        // Set the data to the posted value.
+        foreach ($fields as $field)
+            $this->data[$field] = $this->input->post($field);
+
+        // Lang data
+        $this->lang_data = array();
+
+        $fields = $this->db->list_fields($this->lang_table);
+
+        foreach (Settings::get_languages() as $language) {
+            foreach ($fields as $field) {
+                if ($this->input->post($field . '_' . $language['lang']) !== false) 
+                    $this->lang_data[$language['lang']][$field] = $this->input->post($field . '_' . $language['lang']);
+                
             }
         }
     }
-
-    /**
-     * Update Category
-     */
-    function update($categoryID) {
-        $this->eventcalendar->feed_template($categoryID, $this->template);
-        $this->eventcalendar->feed_lang_template($categoryID, $this->template);
-
-        $this->template['category'] = $this->eventcalendar->getCategory($categoryID);
-
-        $this->output('admin/edit_category');
-    }
-
-    /**
-     * Delete Category
-     */
-    function xhr_delete() {
-        $categoryID = $this->input->post('id_event_category');
-
-        if ($categoryID) {
-            if ($categoryID != '') {
-                $this->eventcalendar->deleteCategory($categoryID);
-
-                $addon_data = array(
-                    'id' => $categoryID
-                );
-
-                $this->success(lang('module_eventcalendar_category_deleted'), $addon_data);
-            } else {
-                $this->error(lang('module_eventcalendar_category_ndeleted'));
-            }
-        } else {
-            $this->error(lang('module_eventcalendar_category_ndeleted'));
-        }
-    }
-
 }
