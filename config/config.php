c23d1d24b5a42e9b2bafc856675d5d8ff84a974a
zachbf <zach.biessefitton@gmail.com>
2016-05-30 18:37:09 +1000

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,22 +1,21 @@
-<?php 
-
-// Module Name
-$config['module_eventcalendar_name'] = 'Event Calendar Module';
-
-// Module Folder
-$config['module_eventcalendar_folder'] = 'Eventcalendar';
-
-// Module Addons Folder
-$config['module_eventcalendar_addons_folder'] = 'admin/eventcalendar_addons/';
-
-// Module Folder Lowercase
-$config['module_eventcalendar_folder_lowercase'] = strtolower($config['module_eventcalendar_folder']);
-
-// Module URL
-$config['module_eventcalendar_url'] = admin_url() . 'module/' . $config['module_eventcalendar_folder_lowercase'] . '/';
-
-// Module Assets Folder
-$config['module_eventcalendar_assets_folder'] = base_url(). 'modules/' . $config['module_eventcalendar_folder'] . '/assets/';
-
-// Module View Folder
-$config['module_eventcalendar_views_folder'] = base_url(). 'modules/' . $config['module_eventcalendar_folder'] . '/views/';
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config['module']['eventcalendar'] = array
+(
+    'module' => "eventcalendar",
+        'name' => "Event Calendar Module",
+        'module_eventcalendar_folder' => 'Eventcalendar',
+        'module_eventcalendar_addons_folder' => 'admin/eventcalendar_addons/',
+        'module_eventcalendar_folder_lowercase' => 'eventcalendar',
+        'module_eventcalendar_url' => admin_url() . 'module/' . 'eventcalendar' . '/',
+        'module_eventcalendar_assets_folder' => base_url(). 'modules/' . 'Eventcalendar' . '/assets/',
+        'module_eventcalendar_views_folder' => base_url(). 'modules/' .'Eventcalendar' . '/views/',
+    'description' => "Author eventcalendar module. Manage articles's authors.<br/>This module is one Demo module, based on the tutorial available on: http://doc.ionizecms.com/en/tutorials",
+    'author' => "",
+    'version' => "",
+   
+    'uri' => 'eventcalendar',
+    'has_admin'=> TRUE,
+    'has_frontend'=> TRUE,
+    
+);
+return $config['module']['eventcalendar'];
